60df43992e1700cba3527886e185a6fc35b92b57
BJ Chris <chrisholic22@gmail.com>
2025-04-14 15:33:02 +0900

    Feat. pixelMap prop 제거

--- app/page.tsx
+++ app/page.tsx
@@ -170,7 +170,6 @@ export default function Home() {
             isOpen={isDialogOpen}
             onClose={() => setIsDialogOpen(false)}
             onPurchase={handlePurchase}
-            pixelMap={pixelMap}
             blockSize={BLOCK_SIZE}
           />
 
