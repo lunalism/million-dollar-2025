8b68e9c8e2b1a6e80bafda687d4eaa7585f31356
BJ Chris <chrisholic22@gmail.com>
2025-04-14 12:15:41 +0900

    Feat. Supabase 설정 변경

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -6,7 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 
 // Supabase 클라이언트 생성
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_KEY || "";
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("Supabase URL and Anon Key must be defined in environment variables.");
